5b5c3c1f652cc4ecf2272f2c8885059d9001bbfe
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-31 13:10:31 +0530

    date picker  and form

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,10 +6,119 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _title = '';
+  String _priority = '';
+  DateTime _date = DateTime.now();
+
+  TextEditingController _dateController = TextEditingController();
+
+  _handleDatePicker() async {
+    final date = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (date != null && date != _date) {
+      setState(() {
+        _date = date;
+      });
+      _dateController.text = _date.toString();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pop(context),
+            child: Icon(
+              Icons.arrow_back_ios,
+              size: 30.0,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Text(
+            "Add Task",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: "Title",
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    validator: (input) {
+                      if (input == null) return input;
+                      return input.trim().isEmpty
+                          ? 'Please enter a title'
+                          : null;
+                    },
+                    onSaved: (input) => input != null ? _title = input : null,
+                    initialValue: _title,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    controller: _dateController,
+                    onTap: _handleDatePicker,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: "Date",
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextFormField(
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      labelText: "Title",
+                      labelStyle: TextStyle(fontSize: 18.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    validator: (input) {
+                      if (input == null) return input;
+                      return input.trim().isEmpty
+                          ? 'Please enter a title'
+                          : null;
+                    },
+                    onSaved: (input) => input != null ? _title = input : null,
+                    initialValue: _title,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
