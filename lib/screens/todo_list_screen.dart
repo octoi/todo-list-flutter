c1b65b258a6e697f977bd1657845ec66d993b708
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-31 08:06:09 +0530

    list view with titles

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -14,6 +14,50 @@ class _TodoListScreenState extends State<TodoListScreen> {
         child: Icon(Icons.add),
         onPressed: () {},
       ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 80.0),
+        physics: BouncingScrollPhysics(),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20.0,
+                horizontal: 40.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "My Tasks",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Text(
+                    "1 of 10",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+            );
+          }
+
+          return Container(
+            margin: EdgeInsets.all(10.0),
+            height: 100.0,
+            color: Colors.red,
+            width: double.infinity,
+          );
+        },
+      ),
     );
   }
 }
