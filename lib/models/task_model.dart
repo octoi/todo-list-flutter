f5b0467e511bc971a6005070b4c3fde3e9afd39d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-01 09:09:17 +0530

    model

---
+++ lib/models/task_model.dart
@@ -0,0 +1,43 @@
+class Task {
+  int id;
+  String title;
+  DateTime date;
+  String priority;
+  int status; // 0 - incomplete, 1 - complete
+
+  Task({
+    required this.title,
+    required this.date,
+    required this.priority,
+    required this.status,
+    this.id = 0,
+  });
+
+  Task.withId({
+    required this.id,
+    required this.title,
+    required this.date,
+    required this.priority,
+    required this.status,
+  });
+
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    map['id'] = id;
+    map['title'] = title;
+    map['date'] = date.toIso8601String();
+    map['priority'] = priority;
+    map['status'] = status;
+    return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task.withId(
+      id: map['id'],
+      title: map['title'],
+      date: DateTime.parse(map['date']),
+      priority: map['priority'],
+      status: map['status'],
+    );
+  }
+}
