c8bea84daaa656bae89b76979fe6721726a696eb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-22 14:52:33 +0530

    ftp

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,9 +1,39 @@
-import 'package:localstorage/localstorage.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:todolist/models/task_model.dart';
 import 'dart:convert';
+import 'dart:io';
 
-final LocalStorage storage = new LocalStorage("tasks");
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path;
+}
 
-saveTasks(List<Task> allTasks) {
-  storage.setItem("tasks", json.encode(allTasks));
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/tasks.json');
+}
+
+Future<File> write(data) async {
+  final file = await _localFile;
+  return file.writeAsString('{tasks: $data}');
+}
+
+Future<String> read() async {
+  try {
+    final file = await _localFile;
+    final contents = await file.readAsString();
+    return contents;
+  } catch (e) {
+    return "";
+  }
+}
+
+saveTasks(List<Task> allTasks) async {
+  await write(jsonEncode(allTasks));
+  await getTasks();
+}
+
+getTasks() async {
+  String contents = await read();
+  print(contents);
 }
