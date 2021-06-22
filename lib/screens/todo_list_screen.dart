5a5bea7384e1a1ad89499dafd9e01b277d52e65e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-22 13:29:57 +0530

    added title in single child scroll view

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -54,13 +54,12 @@ class _TodoListScreenState extends State<TodoListScreen> {
           );
         },
       ),
-      body: ListView.builder(
+      body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 80.0),
         physics: BouncingScrollPhysics(),
-        itemCount: tasks.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0) {
-            return Padding(
+        child: Column(
+          children: [
+            Padding(
               padding: const EdgeInsets.symmetric(
                 vertical: 20.0,
                 horizontal: 40.0,
@@ -87,12 +86,50 @@ class _TodoListScreenState extends State<TodoListScreen> {
                   )
                 ],
               ),
-            );
-          }
-
-          return _buildTask(tasks[index]);
-        },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+// ListView.builder(
+//         padding: EdgeInsets.symmetric(vertical: 80.0),
+//         physics: BouncingScrollPhysics(),
+//         itemCount: tasks.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           if (index == 0) {
+//             return Padding(
+//               padding: const EdgeInsets.symmetric(
+//                 vertical: 20.0,
+//                 horizontal: 40.0,
+//               ),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Text(
+//                     "My Tasks",
+//                     style: TextStyle(
+//                       color: Colors.black,
+//                       fontSize: 40.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                   SizedBox(height: 10.0),
+//                   Text(
+//                     "1 of 10",
+//                     style: TextStyle(
+//                       color: Colors.grey,
+//                       fontSize: 20.0,
+//                       fontWeight: FontWeight.w600,
+//                     ),
+//                   )
+//                 ],
+//               ),
+//             );
+//           }
+
+//           return _buildTask(tasks[index]);
+//         },
+//       ),
